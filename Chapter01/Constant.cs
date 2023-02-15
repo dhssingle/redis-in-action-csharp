41a73f13c99da6df45d1b183820627a16e64a783
dinghs <837527399@qq.com>
2023-02-15 23:14:15 +0800

    refactor

---
+++ Chapter01/Constant.cs
@@ -0,0 +1,8 @@
+namespace Chapter01;
+
+public class Constant
+{
+    public const int OneWeekInSeconds = 7 * 86400;
+    public const int VoteScore = 432;
+    public const int ArticlePerPage = 25;
+}
